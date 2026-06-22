239fe7d1bc60d47eb46b496f7bc672319878a459
johnoopstv-creator <johnoopstv@gmail.com>
2026-06-22 20:28:21 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,9 @@
-const CACHE_NAME = "kel-bek-final-network-first-v2";
+const CACHE_NAME = "kel-bek-last10-named-only-network-first-v1";
 const ASSETS = ["./manifest.json","./icon-192.png","./icon-512.png"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k.startsWith("kel-bek") && k !== CACHE_NAME) return caches.delete(k);
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
+self.addEventListener("install", event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate", event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => {if (k.startsWith("kel-bek") && k !== CACHE_NAME) return caches.delete(k);} ))));self.clients.claim();});
 self.addEventListener("fetch", event => {
-  const req = event.request;
-
-  if (req.mode === "navigate" || req.url.includes("index.html")) {
-    event.respondWith(fetch(req).catch(() => caches.match("./index.html")));
-    return;
-  }
-
-  event.respondWith(
-    fetch(req).catch(() => caches.match(req))
-  );
+ const req=event.request;
+ if(req.mode==="navigate" || req.url.includes("index.html")){event.respondWith(fetch(req).catch(()=>caches.match("./index.html")));return;}
+ event.respondWith(fetch(req).catch(()=>caches.match(req)));
 });
